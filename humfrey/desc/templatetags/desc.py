f9fb3ce84b1c9d725050e94e64d022f08d588c02
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2011-04-06 09:19:21 +0000

    Added property filter (taking a comma-separated list of properties) and made use of it in the ontology template.
    git-svn-id: https://svn.oucs.ox.ac.uk/groups/infodev/projects/dataox/humfrey@705 51206483-761b-46e0-9bf0-6548913a91db

--- humfrey/desc/templatetags/desc.py
+++ humfrey/desc/templatetags/desc.py
@@ -24,6 +24,8 @@ def get_list(r):
 		yield r.rdf_first
 		r = r.rdf_rest
 
+
+
 @register.filter
 def node2(obj):
     if isinstance(obj, BaseResource):
@@ -32,10 +34,14 @@ def node2(obj):
         elif obj.owl_intersectionOf:
             return mark_safe(u'(%s)' % ' &#8746; '.join(map(node2, get_list(obj.get('owl:intersectionOf')))))
         elif isinstance(obj, URIRef):
-            return mark_safe(u'<a href="%s">%s</a>' % (quoteattr(obj.doc_url), escape(obj.label2)))
+            return mark_safe(u'<a href=%s>%s</a>' % (quoteattr(obj.doc_url), escape(obj.label2)))
         else:
             return mark_safe(u'<em>unnamed</em>')
     elif isinstance(obj, Literal):
         return obj.toPython()
     else:
         return unicode(obj)
+
+@register.filter
+def property(obj, value):
+    return obj.get_one_of(*value.split(','))
