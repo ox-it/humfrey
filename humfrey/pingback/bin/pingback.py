91f594c1ba0b2c20123204b0140faa12ef64c6a4
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2011-05-03 14:40:49 +0000

    Tweaks and improvements to the pingback daemon
    git-svn-id: https://svn.oucs.ox.ac.uk/groups/infodev/projects/dataox/humfrey@782 51206483-761b-46e0-9bf0-6548913a91db

--- humfrey/pingback/bin/pingback.py
+++ humfrey/pingback/bin/pingback.py
@@ -1,4 +1,4 @@
-import pickle, base64, urllib, urllib2, sys, threading, Queue, logging, time, rdflib, urlparse, hashlib, socket
+import pickle, base64, urllib, urllib2, sys, threading, logging, time, rdflib, urlparse, hashlib, socket, signal
 from collections import defaultdict
 from functools import wraps
 
@@ -9,7 +9,7 @@ from lxml import etree
 
 WORKER_COUNT = 8
 
-logging.basicConfig(stream=sys.stderr)
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 logger = logging.getLogger('pingback')
 
 RDF_MEDIA_TYPES = {
@@ -18,7 +18,7 @@ RDF_MEDIA_TYPES = {
     'text/turtle': 'n3',
 }
 
-TARGET_HOSTS = frozenset(['data.ox.ac.uk', 'oxpoints.oucs.ox.ac.uk'])
+TARGET_DOMAINS = frozenset(['data.ox.ac.uk', 'oxpoints.oucs.ox.ac.uk'])
 
 SIOC = rdflib.Namespace('http://rdfs.org/sioc/ns#')
 DCTERMS = rdflib.Namespace('http://purl.org/dc/terms/')
@@ -38,6 +38,24 @@ class PutRequest(urllib2.Request):
     def get_method(self):
         return 'PUT'
 
+
+def set_data(client, ping_hash, data):
+    client.set('pingback.data:%s' % ping_hash, base64.b64encode(pickle.dumps(data)))
+def get_data(client, ping_hash):
+    return pickle.loads(base64.b64decode(client.get('pingback.data:%s' % ping_hash)))
+def set_expiry(client, ping_hash):
+    client.expire('pingback.data:%s' % ping_hash, 3600*24*7)
+
+def redis_queue(client, key, bail):
+    while not bail.is_set():
+        item = client.blpop(key, 30)
+        if item is None:
+            continue
+        key_name, ping_hash = item
+        data = get_data(client, ping_hash)
+        
+        yield key_name, ping_hash, data
+
 # Bailing infrastructure
 
 def bailable(f):
@@ -51,13 +69,9 @@ def bailable(f):
             raise
     return g
 
-def bailer(bail, *queues):
-    bail.wait()
-    for queue in queues:
-        queue.put(None)
 
 
-def process(data):
+def process(client, data):
     source, target = (rdflib.URIRef(data[x]) for x in ('source', 'target'))
 
     request = urllib2.Request(source)
@@ -66,10 +80,12 @@ def process(data):
     try:
         response = urllib2.urlopen(request)
     except (ValueError, urllib2.URLError), e:
-        logger.warning('Problem with URL for %r' % data['source'])
+        logger.exception('Problem with URL for %r' % data['source'])
+        data['state'] = 'invalid'
         return
     except urllib2.HTTPError, e:
-        logger.warning('Could not retrieve source: %r' % data['source'])
+        logger.exception('Could not retrieve source: %r' % data['source'])
+        data['state'] = 'invalid'
         return
         
     content_type = response.headers.get('Content-Type', '').split(';')[0].lower()
@@ -86,13 +102,15 @@ def process(data):
     else:
         logger.warning('Unexpected media type for %r: %r' % (data['source'], content_type))
         response.close()
+        data['state'] = 'invalid'
         return
-    
+
     if not graph:
+        data['state'] = 'invalid'
         return
     
     graph_name = rdflib.URIRef('http://data.ox.ac.uk/graph/pingback/' + data['hash'])
-    
+
     graph_metadata += (
         (graph_name, DCTERMS.created, rdflib.Literal(data['date'])),
         (graph_name, DCTERMS.source, rdflib.URIRef(response.geturl())),
@@ -105,7 +123,16 @@ def process(data):
     data['graph_name'] = graph_name
     data['hostname'] = socket.gethostbyaddr(data['remote_addr'])[0]
     
-    return data
+    if client.sismember('pingback.whitelist.ip', data['remote_addr']) or \
+       client.sismember('pingback.whitelist.hostname', data['hostname']) or \
+       client.sismember('pingback.whitelist.source_domain', data['source_domain']):
+        data['state'] = 'accepted'
+    elif client.sismember('pingback.blacklist.ip', data['remote_addr']) or \
+         client.sismember('pingback.blacklist.hostname', data['hostname']) or \
+         client.sismember('pingback.blacklist.source_domain', data['source_domain']):
+        data['state'] = 'rejected'
+    else:
+        data['state'] = 'pending'
 
 def process_html(source, target, response, graph):
     html = etree.parse(response, parser=etree.HTMLParser())
@@ -148,20 +175,21 @@ def process_rdf(source, target, response, graph, format):
             add_labels(o) 
     
 @bailable
-def worker(client_locks, host_locks, queue_new, queue_pending, bail):
-    while not bail.is_set():
-        data = queue_new.get()
-        if data is None:
-            queue_new.put(None)
-            break
+def worker(client_locks, host_locks, bail):
+    client = get_redis_client()
+    
+    for _, ping_hash, data in redis_queue(client, 'pingback.new', bail):
         
         source_url = urlparse.urlparse(data['source'])
-        source_host = source_url.netloc.split(':')[0].lower()
+        source_domain = source_url.netloc.split(':')[0].lower()
         
         target_url = urlparse.urlparse(data['target'])
-        target_host = source_url.netloc.split(':')[0].lower()
+        target_domain = source_url.netloc.split(':')[0].lower()
         
-        if target_host not in TARGET_HOSTS:
+        data['source_domain'] = source_domain
+        data['target_domain'] = target_domain
+        
+        if target_domain not in TARGET_DOMAINS:
             logging.warning('Pingback for non-targetable host: %r' % data['target'])
             continue
         if source_url.scheme not in ('http', 'https', 'ftp'):
@@ -170,35 +198,30 @@ def worker(client_locks, host_locks, queue_new, queue_pending, bail):
             
         
         client_lock = client_locks[data['remote_addr']]
-        host_lock = host_locks[source_host]
+        host_lock = host_locks[source_domain]
         
         with client_lock, host_lock:
-            data = process(data)
-            if data is not None:
-                queue_pending.put(data)
+            print id(data)
+            process(client, data)
+            set_data(client, ping_hash, data)
+            print id(data)
+            logging.info('Data with state %r' % data['state'])
+            if data['state'] in ('invalid', 'rejected'):
+                set_expiry(client, ping_hash)
+            elif data['state'] == 'accepted':
+                client.rpush('pingback.accepted', ping_hash)
+            elif data['state'] == 'pending':
+                client.sadd('pingback.pending', ping_hash)
+            else:
+            	logging.error('Unexpected state')
+                
             time.sleep(1)
-        
 
 @bailable
-def pending_manager(queue_pending, bail):
+def accepted_manager(bail):
     client = get_redis_client()
     
-    while not bail.is_set():
-        data = queue_pending.get()
-        if data is None:
-            break
-        data['state'] = 'pending'
- 
-        client.set('pingback.data:%s' % data['hash'], base64.b64encode(pickle.dumps(data)))
-        client.sadd('pingback.pending', data['hash'])
-
-def accepted_manager(queue_accepted, bail):
-    client = get_redis_client()
-    
-    while not bail.is_set():
-        data = queue_accepted.get()
-        if data is None:
-            break
+    for _, ping_hash, data in redis_queue(client, 'pingback.accepted', bail):
         
         graph = data['graph']
         graph += data['graph_metadata']
@@ -211,57 +234,44 @@ def accepted_manager(queue_accepted, bail):
         except urllib2.HTTPError, e:
             if e.code not in (201, 204,):
                 raise
+        
+        data['state'] = 'published'
+        set_data(client, ping_hash, data)
+        set_expiry(client, ping_hash)
+
+def interrupt_handler(bail):
+    def f(signum, frame):
+        logging.info("Bailing")
+        bail.set()
+    return f
+	
+
 def run():
+    #signal.signal(signal.SIGINT, interrupt_handler)
+
     client_locks, host_locks = defaultdict(threading.Lock), defaultdict(threading.Lock)
     bail = threading.Event()
-    queue_new, queue_pending, queue_accepted = (Queue.Queue() for i in range(3))
     
     worker_pool = []
     for i in range(WORKER_COUNT):
         worker_thread = threading.Thread(target=worker,
-                                         args=(client_locks, host_locks,
-                                               queue_new, queue_pending),
-                                         kwargs={'bail': bail}) 
+                                         args=(client_locks, host_locks),
+                                         kwargs={'bail': bail})
+        #worker_thread.daemon = True 
         worker_thread.start()
         worker_pool.append(worker_thread)
         
-    bail_thread = threading.Thread(target=bailer, args=(bail, queue_new, queue_pending))
-    bail_thread.start()
-    
-    pending_manager_thread = threading.Thread(target=pending_manager,
-                                              args=(queue_pending,),
-                                              kwargs={'bail': bail})
-    pending_manager_thread.start()
-
     accepted_manager_thread = threading.Thread(target=accepted_manager,
-                                               args=(queue_accepted,),
                                                kwargs={'bail': bail})
+    #accepted_manager_thread.daemon = True 
     accepted_manager_thread.start()
 
-    client = get_redis_client()
-
     try:
-        while True:
-            item = client.blpop(('pingback.new', 'pingback.accepted'), 30)
-            if item is None:
-                continue
-            key_name, ping_hash = item
-            data = client.get('pingback.data:%s' % ping_hash)
-            try:
-                data = pickle.loads(base64.b64decode(data))
-            except Exception, e:
-                logger.exception('Failed to decode pingback data.\n')
-                continue
-
-            if key_name == 'pingback.new':
-                queue_new.put(data)
-            elif key_name == 'pingback.accepted':
-                queue_accepted.put(data)
-    except:
-        bail.set()
-        raise
-    #finally:
-    #    client.close()
+        while True: time.sleep(100)
+    except KeyboardInterrupt:
+    	bail.set()
+    
+    logging.info("Shutting down")
 
 
 if __name__ == '__main__':
