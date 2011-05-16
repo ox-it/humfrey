f3f3160a8acb00a394c9a20dca1554fd4348ee40
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2011-05-16 10:09:58 +0100

    Simplified the default SPARQL query.

--- humfrey/desc/forms.py
+++ humfrey/desc/forms.py
@@ -5,7 +5,7 @@ def SparqlQueryForm(*args, **kwargs):
     formats = sorted([(format, f.name) for (format, f) in formats.iteritems()])
 
     class _SparqlQueryForm(forms.Form):
-        query = forms.CharField(widget=forms.Textarea(), initial="SELECT DISTINCT ?type WHERE {\n  ?thing a ?type\n} LIMIT 50")
+        query = forms.CharField(widget=forms.Textarea(), initial="PREFIX owl: <http://www.w3.org/2002/07/owl#>\n\nSELECT DISTINCT ?type WHERE {\n  ?type a owl:Class\n} LIMIT 50")
         format = forms.ChoiceField(choices=formats, initial='html', required=False)
         common_prefixes = forms.BooleanField(label='Assume common namespace prefixes', initial=False, required=False, help_text="When checked, common prefix declarations are prepended to your query.")
     return _SparqlQueryForm(*args, **kwargs)
