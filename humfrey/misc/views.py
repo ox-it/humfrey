3196c0cbb4dd7e6d631e011e04a487e14bf7d712
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2011-07-31 10:43:00 +0100

    Added SimpleView for simple access to the renderer.

--- humfrey/misc/views.py
+++ humfrey/misc/views.py
@@ -34,3 +34,15 @@ else:
             if template.endswith('.html'):
                 template = template[:-5]
             return self.render(request, context, 'index')
+
+
+class SimpleView(BaseView):
+    def __init__(self, template_name, context=None):
+        self._context = context or {}
+        self._template_name = template_name
+        super(SimpleView, self).__init__()
+    def initial_context(self, request):
+        return self._context
+    def handle_GET(self, request, context):
+        return self.render(request, context, self._template_name)
+        
