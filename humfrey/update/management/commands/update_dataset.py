dc755e07516e1cbbcd01f01e8be59abf8f1a6329
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2011-09-24 14:49:45 +0100

    Update trigger can now be specified on the command line as the second argument, and the module can now be run as a script.

--- humfrey/update/management/commands/update_dataset.py
+++ humfrey/update/management/commands/update_dataset.py
@@ -14,6 +14,7 @@ from humfrey.update.longliving.updater import Updater
 class Command(BaseCommand):
     def handle(self, *args, **options):
         config_filename = os.path.abspath(args[0])
+        trigger = args[1] if len(args) > 1 else 'manual'
         
         with open(config_filename, 'r') as f:
             config_file = etree.parse(f)
@@ -25,7 +26,10 @@ class Command(BaseCommand):
         client.rpush(Updater.QUEUE_NAME, base64.b64encode(pickle.dumps({
             'config_filename': config_filename,
             'name': dataset_name,
-            'trigger': 'manual',
+            'trigger': trigger,
             'queued_at': datetime.datetime.now(),
         })))
 
+if __name__ == '__main__':
+    import sys
+    Command().handle(*sys.argv[1:])
