b67128b55e27435211e91896889e66e57014742a
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2011-09-25 21:56:38 +0100

    Better handling of 201 and 204 response codes, and some whitespace squelching.

--- humfrey/update/longliving/uploader.py
+++ humfrey/update/longliving/uploader.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import traceback
 import urllib
 import urllib2
 
@@ -15,39 +16,41 @@ class Uploader(LonglivingThread):
 
     def run(self):
         client = self.get_redis_client()
-        
+
         for _, item in self.watch_queue(client, self.QUEUE_NAME, True):
             self.process_item(client, item)
-    
+
     def process_item(self, client, item):
         graph_url = '%s?%s' % (settings.ENDPOINT_GRAPH,
                                urllib.urlencode({'graph': item['graph_name']}))
-        
+
         if 'filename' in item:
             data = open(item['filename']).read()
         elif 'graph' in item:
             data = item['graph'].serialize()
         elif 'data' in item:
             data = item['data']
-        
+
         request = urllib2.Request(graph_url, data)
         request.headers['Content-Type'] = item.get('mimetype', 'text/plain')
         request.get_method = lambda: item['method']
-        
+
         try:
             urllib2.urlopen(request)
         except Exception, e:
-            item.update({
-                'outcome': 'error',
-                'error': repr(e),
-            })
-            logger.exception("Upload failed. Code %r", getattr(e, 'code', None))
+            if isinstance(e, urllib2.HTTPError) and e.code in (201, 204):
+                item['outcome'] = 'success'
+            else:
+                item.update({'outcome': 'error',
+                             'error': repr(e),
+                             'traceback': traceback.format_exc()})
+                logger.exception("Upload failed. Code %r", getattr(e, 'code', None))
         else:
             item.update({
                 'outcome': 'success',
             })
-            
+
         if 'filename' in item and item.get('delete_after'):
             os.unlink(item['filename'])
-        
+
         client.publish(self.UPLOADED_PUBSUB, self.pack(item))
