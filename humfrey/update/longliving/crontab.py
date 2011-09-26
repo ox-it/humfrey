4aff03b2933b2aca8bc26402ef5efe135af1a173
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2011-09-26 10:57:39 +0100

    Fixed crontab maintainer and its subprocess interactions with `crontab`.

--- humfrey/update/longliving/crontab.py
+++ humfrey/update/longliving/crontab.py
@@ -1,7 +1,10 @@
+from __future__ import with_statement
+
 import logging
 import os
 import subprocess
 import sys
+import tempfile
 
 from lxml import etree
 
@@ -13,13 +16,13 @@ logger = logging.getLogger(__name__)
 class CrontabMaintainer(LonglivingThread):
     CRON_DEFINITIONS = 'update:crontab:definitions'
     CRON_LINE = '%(pattern)s HUMFREY_CONFIG_FILE=%(humfrey_config_file)s' \
-                         + ' DJANGO_SETTINGS_MODULE=%(django_setings_module)s' \
+                         + ' DJANGO_SETTINGS_MODULE=%(django_settings_module)s' \
                          + ' %(executable)s ' \
                          + ' -m humfrey.update.management.commands.update_dataset' \
                          + ' %(filename)s crontab\n'
     TOP_SENTINAL = '# BEGIN maintained by humfrey crontab maintainer\n'
     BOTTOM_SENTINAL = '# END maintained by humfrey crontab maintainer\n'
-    
+
     def run(self):
         client = self.get_redis_client()
         pubsub = client.pubsub()
@@ -36,23 +39,21 @@ class CrontabMaintainer(LonglivingThread):
         finally:
             pubsub.unsubscribe(Definitions.UPDATED_CHANNEL)
             pubsub.unsubscribe(LonglivingThread.BAIL_CHANNEL)
-    
+
     def perform_update(self, client, changed):
         cron_definitions = client.get(self.CRON_DEFINITIONS)
         cron_definitions = self.unpack(cron_definitions) if cron_definitions else {}
         for id, filename, xml in changed:
             xml = etree.fromstring(xml)
             pattern = xml.xpath('/update-definition/trigger/cron/@pattern')
-            print pattern
             if not pattern and id in cron_definitions:
                 del cron_definitions[id]
             elif pattern:
                 cron_definitions[id] = {'id': id,
                                         'filename': filename,
                                         'pattern': unicode(pattern[0])}
-        print cron_definitions
         client.set(self.CRON_DEFINITIONS, self.pack(cron_definitions))
-        
+
         crontab_lines = []
         for _, definition in sorted(cron_definitions.iteritems()):
             line = self.CRON_LINE % {
@@ -63,13 +64,13 @@ class CrontabMaintainer(LonglivingThread):
                 'filename': definition['filename'],
             }
             crontab_lines.append(line)
-        
+
         crontab_process = subprocess.Popen(['crontab', '-l'], stdout=subprocess.PIPE)
         if crontab_process.wait() == 0:
-            crontab = crontab_process.read()
+            crontab = crontab_process.stdout.readlines()
         else:
             crontab = []
-        
+
         try:
             insert_top = crontab.index(self.TOP_SENTINAL) + 1
             insert_bottom = crontab.index(self.BOTTOM_SENTINAL, insert_top)
@@ -78,16 +79,11 @@ class CrontabMaintainer(LonglivingThread):
                 return # No lines to insert, and there's nothing there anyway.
             crontab.extend(['\n', self.TOP_SENTINAL, self.BOTTOM_SENTINAL])
             insert_top, insert_bottom = -1, -1
-        
+
         crontab[insert_top:insert_bottom] = crontab_lines
-        
-        crontab_process = subprocess.Popen(['crontab'], stdin=subprocess.PIPE)
-        for line in crontab:
-            crontab_process.stdin.write(line)
-        crontab_process.stdin.close()
-        crontab_process.wait()
-            
-        
-        
-        
+
+        with tempfile.NamedTemporaryFile() as f:
+            f.write(''.join(crontab))
+            f.flush()
+            subprocess.call(['crontab', f.name])
         
