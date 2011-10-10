554d13a4d7196ec93bc34c116e54436cc9ed8e8d
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2011-10-10 14:28:51 +0100

    Added a CannedQueryView to humfrey.misc.views.

--- humfrey/misc/views.py
+++ humfrey/misc/views.py
@@ -1,10 +1,11 @@
 import datetime
 
+import rdflib
 from django.conf import settings
 
-from django_conneg.views import HTMLView
+from django_conneg.views import HTMLView, ContentNegotiatedView
 
-from humfrey.utils.views import CachedView
+from humfrey.utils.views import CachedView, EndpointView
 
 # Only create FeedView class if feedparser and pytz are importable.
 # This will make feedparser and pytz optional dependencies if one
@@ -42,3 +43,38 @@ class SimpleView(HTMLView, CachedView):
 
     def get(self, request):
         return self.render(request, self.context.copy(), self.template_name)
+
+
+class CannedQueryView(CachedView, EndpointView):
+    query = None
+    template_name = None
+
+    def get_query(self, request, *args, **kwargs):
+        return self.query
+
+    def get_locations(self, request, *args, **kwargs):
+        """
+        Override to provide the canonical and format-specific locations for this resource.
+        
+        Example return value: ('http://example.com/data', 'http://example.org/data.rss')
+        """
+        return None, None
+    
+    def get(self, request, *args, **kwargs):
+        self.base_location, self.content_location = self.get_locations(request, *args, **kwargs)
+        query = self.get_query(request, *args, **kwargs)
+        result = self.endpoint.query(query)
+        if isinstance(result, list):
+            context = {'results': result}
+        elif isinstance(result, bool):
+            context = {'result': result}
+        elif isinstance(result, rdflib.ConjunctiveGraph):
+            context = {'graph': result}
+        
+        if self.content_location:
+            context['additional_headers'] = {'Content-location': self.content_location}
+            
+        if 'format' in kwargs:
+            self.render_to_format(request, context, self.template_name, kwargs['format'])
+        else: 
+            return self.render(request, context, self.template_name)
