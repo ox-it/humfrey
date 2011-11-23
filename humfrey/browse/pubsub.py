94c4da6f605cf35780138bb3d3ed2ffd6921e6c6
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2011-11-23 13:33:11 +0000

    Fixed bug that caused failure when BROWSE_LISTS (an optional setting) wasn't set.

--- humfrey/browse/pubsub.py
+++ humfrey/browse/pubsub.py
@@ -6,7 +6,7 @@ from humfrey.browse import update
 
 @pubsub_watcher(channel=Updater.UPDATED_CHANNEL, priority=100)
 def update_list(client, channel, data):
-    browse_lists = getattr(settings, 'BROWSE_LISTS') or ()
+    browse_lists = getattr(settings, 'BROWSE_LISTS', None) or ()
 
     for browse_list in browse_lists:
         if data['slug'] in browse_list.get('update_triggered_by', ()):
