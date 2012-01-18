f887067111b7be3d906678e1b7c842914e408a6b
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2012-01-18 13:37:48 +0000

    Moved to django-object-permissions for row-level permissions in update app.

--- humfrey/update/models.py
+++ humfrey/update/models.py
@@ -1,9 +1,11 @@
 import datetime
 
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django_longliving.util import pack, get_redis_client
+from object_permissions import register
 
 from humfrey.update.utils import evaluate_pipeline
 
@@ -12,6 +14,9 @@ DEFINITION_STATUS_CHOICES = (
     ('queued', 'Queued'),
     ('active', 'Active'),
 )
+
+permission_check = lambda name : (lambda self, user : user.has_perm('update.%s_updatedefinition' % name)
+                                                   or user.has_perm(name, self))
 
 class UpdateDefinition(models.Model):
     UPDATE_QUEUE = 'humfrey:update:update-queue'
@@ -25,11 +30,6 @@ class UpdateDefinition(models.Model):
 
     cron_schedule = models.TextField(blank=True)
 
-    view_users = models.ManyToManyField(User, related_name="update_definitions_view", blank=True)
-    change_users = models.ManyToManyField(User, related_name="update_definitions_change", blank=True)
-    execute_users = models.ManyToManyField(User, related_name="update_definitions_execute", blank=True)
-    delete_users = models.ManyToManyField(User, related_name="update_definitions_delete", blank=True)
-
     status = models.CharField(max_length=10, choices=DEFINITION_STATUS_CHOICES, default='idle')
 
     last_queued = models.DateTimeField(null=True, blank=True)
@@ -42,27 +42,15 @@ class UpdateDefinition(models.Model):
             ("admin", "May use the dataset update admin pages"),
             ("view_updatedefinition", "Can view the update definition"),
             ("execute_updatedefinition", "Can perform an update"),
+            ("administer_updatedefinition", "Can administer an update definition"),
         )
 
-    def can_view(self, user):
-        if user.has_perm("update.view_updatedefinition"):
-            return True
-        return bool(UpdateDefinition.objects.filter(pk=self.pk, view_users=user).count())
-
-    def can_change(self, user):
-        if user.has_perm("update.change_updatedefinition" if self.pk else "update.add_updatedefinition"):
-            return True
-        return bool(UpdateDefinition.objects.filter(pk=self.pk, change_users=user).count())
-
-    def can_execute(self, user):
-        if user.has_perm("update.execute_updatedefinition"):
-            return True
-        return bool(UpdateDefinition.objects.filter(pk=self.pk, execute_users=user).count())
-
-    def can_delete(self, user):
-        if user.has_perm("update.delete_updatedefinition"):
-            return True
-        return bool(UpdateDefinition.objects.filter(pk=self.pk, delete_users=user).count())
+    can_view = permission_check('view')
+    can_change = permission_check('change')
+    can_execute = permission_check('execute')
+    can_delete = permission_check('delete')
+    can_administer = permission_check('administer')
+    receives_notifications = permission_check('notifications')
 
     def queue(self, trigger, user=None, silent=False):
         if self.status != 'idle':
@@ -83,6 +71,13 @@ class UpdateDefinition(models.Model):
 
     def get_absolute_url(self):
         return reverse('update:definition-detail', args=[self.slug])
+
+register(['update.administer_updatedefinition',
+          'update.view_updatedefinition',
+          'update.change_updatedefinition',
+          'update.execute_updatedefinition',
+          'update.delete_updatedefinition',
+          'update.notifications_updatedefinition'], UpdateDefinition)
 
 class UpdateLog(models.Model):
     update_definition = models.ForeignKey(UpdateDefinition, related_name="update_log")
@@ -105,7 +100,9 @@ class UpdatePipeline(models.Model):
             raise ValueError(e)
         return super(UpdatePipeline, self).save(*args, **kwargs)
 
-class UpdateVariable(models.Model):
-    update_definition = models.ForeignKey(UpdateDefinition, related_name="variables")
-    name = models.TextField()
-    value = models.TextField()
+class LocalFile(models.Model):
+    name = models.CharField(max_length=255, unique=True, db_index=True)
+    content = models.FileField(upload_to=settings.UPDATE_FILES_DIRECTORY)
+    publish = models.BooleanField(default=True)
+
+register(['update.read_localfile','update.modify_localfile','update.delete_localfile'], LocalFile)
