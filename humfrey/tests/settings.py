d4f947a57d0f7d59b2ffc1355ac0ac999c0afe05
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2012-02-11 12:59:27 +0000

    Remove dependency on django_jenkins for test running; optional if available.

--- humfrey/tests/settings.py
+++ humfrey/tests/settings.py
@@ -1,3 +1,4 @@
+import imp
 import os
 
 os.environ['HUMFREY_CONFIG_FILE'] = os.path.join(os.path.dirname(__file__), 'data', 'config.ini')
@@ -8,17 +9,21 @@ ROOT_HOSTCONF = 'humfrey.tests.hosts'
 DEFAULT_HOST = 'empty'
 ROOT_URLCONF = 'humfrey.tests.urls.empty'
 
-INSTALLED_APPS += (
-    'django_jenkins',
-)
+DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3'}}
 
-JENKINS_TEST_RUNNER = 'humfrey.tests.HumfreyJenkinsTestSuiteRunner'
-JENKINS_TASKS = ('django_jenkins.tasks.run_pylint',
-                 'django_jenkins.tasks.with_coverage',
-                 'django_jenkins.tasks.django_tests',
-                 'django_jenkins.tasks.run_pep8',
-                 'django_jenkins.tasks.run_pyflakes')
-PROJECT_APPS = tuple(app for app in INSTALLED_APPS if app.startswith('humfrey.'))
+try:
+    imp.find_module('django_jenkins')
+except ImportError:
+    pass
+else:
+    INSTALLED_APPS += ('django_jenkins',)
+
+    JENKINS_TEST_RUNNER = 'humfrey.tests.jenkins.HumfreyJenkinsTestSuiteRunner'
+    JENKINS_TASKS = ('django_jenkins.tasks.run_pylint',
+                     'django_jenkins.tasks.with_coverage',
+                     'django_jenkins.tasks.django_tests',
+                     'django_jenkins.tasks.run_pep8',
+                     'django_jenkins.tasks.run_pyflakes')
 
 TEST_URI = 'http://data/example.com/id/Foo'
 TEST_DOMAIN = 'data.example.org'
