0fb15cd8aa8f8649d415cd6b1a8f07660a61f1a8
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2012-02-26 23:06:59 +0000

    Added ability to import update definitions from the filesystem.

---
+++ humfrey/update/management/commands/import_update_definition.py
@@ -0,0 +1,64 @@
+import itertools
+import os
+
+from lxml import etree
+
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+from django.core.files import File
+
+from humfrey.update.models import UpdateDefinition, UpdatePipeline, LocalFile
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        path = args[0]
+
+        definition_filename = os.path.join(path, 'meta.xml')
+        with open(definition_filename, 'r') as f:
+            meta = etree.parse(definition_filename)
+
+        owner = meta.xpath('/meta/owner')[0].text
+
+        for definition in meta.xpath('/meta/definition'):
+            self.import_definition(definition, owner)
+
+        for filename in os.listdir(path):
+            if filename == 'meta.xml' or filename.startswith('.'):
+                continue
+            local_file, _ = LocalFile.objects.get_or_create(name=filename)
+            if local_file.content.file:
+                local_file.content.delete()
+            with open(os.path.join(path, filename)) as src:
+                local_file.content.save(filename, File(src))
+            print local_file.content.file.name
+
+    def import_definition(self, meta, default_owner):
+        slug = meta.xpath('slug')[0].text
+
+        try:
+            definition = UpdateDefinition.objects.get(slug=slug)
+        except UpdateDefinition.DoesNotExist:
+            definition = UpdateDefinition(slug=slug)
+
+        for name in ('title', 'description', 'cron_schedule'):
+            field = meta.xpath(name)
+            setattr(definition, name, field[0].text if field else '')
+
+        owner = meta.xpath('owner')
+        owner = owner[0].text if owner else default_owner
+        definition.owner = User.objects.get(username=owner)
+
+        definition.save()
+
+        # Update pipelines
+        current = definition.pipelines.all()
+        desired = [p.text for p in meta.xpath('pipelines/pipeline')]
+        for obj, value in itertools.izip_longest(current, desired):
+            if not obj:
+                obj = UpdatePipeline(update_definition=definition)
+            if value:
+                obj.value = value
+                obj.save()
+            else:
+                obj.delete()
+
