6f324693cbb98826bc29b8a71a053bbb6248b256
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2012-03-08 09:26:45 +0000

    Added django-conneg as an INSTALLED_APP.

--- humfrey/settings/common.py
+++ humfrey/settings/common.py
@@ -93,6 +93,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.staticfiles',
     'django_hosts',
+    'django_conneg',
     'object_permissions',
     'humfrey.base',
     'humfrey.desc',
