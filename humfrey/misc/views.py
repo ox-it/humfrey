c520bb9fd9e3457afc9ba7713ce6adc2dd9b5686
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2012-05-10 15:52:28 +0100

    Added a PassThroughView, for passing requests through to other systems.

--- humfrey/misc/views.py
+++ humfrey/misc/views.py
@@ -1,7 +1,11 @@
 import datetime
+import httplib
+import urlparse
 
 from django.conf import settings
-
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponse
+from django.views.generic import View
 from django_conneg.views import HTMLView
 
 from humfrey.sparql.endpoint import EndpointView, SparqlResultList, SparqlResultBool, SparqlResultGraph
@@ -91,3 +95,44 @@ class CannedQueryView(EndpointView):
             return self.render_to_format(request, context, self.template_name, request.GET['format'])
         else:
             return self.render(request, context, self.template_name)
+
+class PassThroughView(View):
+    def get_target_url(self, request, *args, **kwargs):
+        raise NotImplementedError
+    def get_method(self, request, *args, **kwargs):
+        return request.method
+    def get_headers(self, request, *args, **kwargs):
+        headers = {}
+        for name in request.META:
+            if name.startswith('HTTP_'):
+                headers[name[5:].capitalize().replace('_', '-')] = request.META[name]
+        return headers
+
+    def get(self, request, *args, **kwargs):
+        url = self.get_target_url(request, *args, **kwargs)
+        url = urlparse.urlparse(url)
+        
+        if ':' in url.netloc:
+            host, port = url.netloc.split(':', 1)
+            port = int(port)
+        else:
+            host, port = url.netloc, {'http': 80, 'https': 443}[url.scheme]
+        path = urlparse.urlunparse(url._replace(scheme='', netloc=''))
+
+        connection_class = httplib.HTTPConnection if url.scheme == 'http' else httplib.HTTPSConnection
+
+        conn = connection_class(host=host, port=port)
+        conn.request(method=self.get_method(request, *args, **kwargs),
+                     url=path,
+                     body=request,
+                     headers=self.get_headers(request, *args, **kwargs))
+        
+        http_response = conn.getresponse()
+        
+        response = HttpResponse(http_response.fp)
+        response.status_code = http_response.status
+        for key, value in http_response.getheaders():
+            response[key] = value
+        return response
+        
+        
