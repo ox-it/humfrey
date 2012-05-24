3098f2b1ef1a8959c7caa231c3f10787324d2ba7
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2012-05-24 21:30:05 +0100

    Removed obsolete humfrey.browse app.

--- humfrey/browse/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-
-from . import views
-
-urlpatterns = patterns('',
-    (r'^$', views.IndexView.as_view(), {}, 'index'),
-    (r'^(?P<id>[a-z\d-]+)/$', views.ListView.as_view(), {}, 'list'),
-)
