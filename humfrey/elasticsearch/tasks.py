60323d702f4e8178b1fbd9a3747fc752032e8ef7
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2012-06-23 22:28:21 +0100

    elasticsearch task now sets last_started and last_completed.

--- humfrey/elasticsearch/tasks.py
+++ humfrey/elasticsearch/tasks.py
@@ -1,3 +1,5 @@
+import datetime
+
 from celery.task import task
 
 from .models import Index
@@ -13,10 +15,12 @@ def update_index(index):
     if isinstance(index, basestring):
         index = Index.objects.get(slug=index)
     index.status = 'active'
+    index.last_started = datetime.datetime.now()
     index.save()
     try:
         index_updater = IndexUpdater()
         index_updater.update(index)
     finally:
         index.status = 'idle'
+        index.last_completed = datetime.datetime.now()
         index.save()
