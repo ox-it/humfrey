e2fd6a662153f14053244bc28ba6482305ca58b7
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2012-07-19 13:55:56 +0100

    Removed pingback admin urls and moved public URLs up a level.

--- humfrey/pingback/urls/admin.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import patterns, url
-
-from humfrey.pingback import views
-
-urlpatterns = patterns('',
-    url(r'^moderation/$', views.ModerationView.as_view(), name='moderation'),
-)
