4d545febc3d0d1a07789ad1b7df13ed1f842154a
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2012-10-02 15:47:53 +0100

    Tidied up humfrey.streaming.RDFSource (docstring, exception message)

--- humfrey/streaming/__init__.py
+++ humfrey/streaming/__init__.py
@@ -3,11 +3,18 @@ import os.path
 from .ntriples import NTriplesSource, NTriplesSink
 from .rdfxml import RDFXMLSource, RDFXMLSink
 
+_source_types = {'.nt': NTriplesSource,
+                 '.rdf': RDFXMLSource}
+
 def RDFSource(source):
+    """
+    Returns an iterator over the triples encoded in source, based on the
+    file extension in source.name.
+    """
     name, ext = os.path.splitext(source.name)
-    if ext == '.nt':
-        return NTriplesSource(source)
-    elif ext == '.rdf':
-        return RDFXMLSource(source)
+    if ext in _source_types:
+        return _source_types[ext](source)
     else:
-        raise AssertionError
+        raise AssertionError("File did not have an expected extension. " +
+                             "Was {0}'; should be one of {1}".format(ext,
+                                                                     ', '.join(_source_types)))
