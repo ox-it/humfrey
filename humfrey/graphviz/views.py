7bc353a4e0340b4bd6293360cc7619ca05c377c7
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2012-10-19 10:52:50 +0100

    Fixed GraphViz view; can now show everything of a type, not just a rooted tree.

--- humfrey/graphviz/views.py
+++ humfrey/graphviz/views.py
@@ -7,68 +7,106 @@ from django.template import RequestContext, loader
 from django.template.defaultfilters import slugify
 from django.shortcuts import render_to_response
 
+from django_conneg.http import HttpBadRequest
 from django_conneg.decorators import renderer
 
 from humfrey.results.views.standard import RDFView
+from humfrey.sparql.views.core import StoreView
+from humfrey.linkeddata.views import MappingView
 from humfrey.linkeddata.resource import Resource
 from humfrey.utils.namespaces import expand, NS
 
-class GraphVizView(RDFView):
-    _QUERY = """
-      CONSTRUCT {
-        %(page_uri)s foaf:topic ?entity .
-        ?entity a ?type %(propertyTriples)s .
-          %(relationPattern)s .
-      } WHERE {
-        { SELECT DISTINCT ?entity WHERE { %(subject)s (%(relationAlternation)s){0,%(depth)d} %(object)s } } .
+class GraphVizView(RDFView, StoreView, MappingView):
+    query = """
+      CONSTRUCT {{
+        {page_uri} foaf:topic ?entity .
+        ?entity a ?type ;
+          rdfs:label ?title .
+        {relationPattern}
+      }} WHERE {{
+        {{
+          SELECT DISTINCT ?entity WHERE {{
+            {selector}
+          }} LIMIT 10000
+        }} 
         ?entity a ?type .
-        OPTIONAL { %(relationPattern)s } .
-        %(propertyPatterns)s
-        OPTIONAL { ?entity rdfs:label ?title } .
-        FILTER (?relation in (%(relationList)s)) .
-        %(excludeTypesFilter)s
-      }
+        OPTIONAL {{
+          VALUES ?relation {{ {relations} }}
+          {relationPattern}
+        }} .
+        OPTIONAL {{ ?entity rdfs:label|dc:title|skos:prefLabel|foaf:name ?title }} .
+        NOT EXISTS {{
+          VALUES ?excludedType {{ {excludedTypes} }}
+          ?entity a ?excludedType
+        }}
+      }}
     """
     
-    def get(self, request, root=None, relations=None, template='graphviz/graphviz', depth=4, max_depth=5, exclude_types=None, properties=None, inverted=None, minimal=None):
-        root = expand(root or request.GET.get('root', ''))
+    type_selector = """
+            ?orgType rdfs:subClassOf* {baseType}
+            GRAPH {graph} {{ ?entity a ?orgType }}
+    """
+    
+    tree_selector = """
+            {subject} {relationAlternation}{{0,{depth}}} {object}
+    """
+    
+    def get(self, request, root=None, base_type=None, graph=None, relations=None, template='graphviz/graphviz', depth=4, max_depth=5, excluded_types=None, properties=None, inverted=None, minimal=None):
+        make_uriref = lambda uri: expand(uri) if uri else None
+        root = make_uriref(root or request.GET.get('root'))
+        base_type = make_uriref(base_type or request.GET.get('base_type'))
+        graph = make_uriref(graph or request.GET.get('graph'))
+
         relations = relations or [expand(relation) for relation in request.GET.getlist('relation')]
-        exclude_types = exclude_types or [expand(t) for t in request.GET.getlist('exclude_type')]
-        properties = properties or [expand(p) for p in request.GET.getlist('property')]
-        try:
-            depth = min(int(request.GET.get('depth', depth)), max_depth)
-        except (TypeError, ValueError):
-            return HttpResponseBadRequest()
-        
         inverted = inverted if (inverted is not None) else request.GET.get('inverted') == 'true'
         minimal = minimal if (minimal is not None) else request.GET.get('minimal') == 'true'
 
+        if not relations:
+            raise Http404
+
         if inverted:
-            subj, obj = '?entity', root.n3()
             relation_pattern = '?entity ?relation ?parent'
         else:
-            subj, obj = root.n3(), '?entity'
             relation_pattern = '?parent ?relation ?entity'
-        
-        types = self.get_types(root)
-        if not types or not relations:
-            raise Http404
-        
+
+        if root and base_type:
+            raise HttpBadRequest
+        elif root:
+            if not self.get_types(root):
+                raise Http404
+
+            if inverted:
+                subj, obj = '?entity', root.n3()
+            else:
+                subj, obj = root.n3(), '?entity'
+
+            try:
+                depth = min(int(request.GET.get('depth', depth)), max_depth)
+            except (TypeError, ValueError):
+                return HttpResponseBadRequest()
+            selector = self.tree_selector.format(subject=subj,
+                                                 object=obj,
+                                                 depth=depth,
+                                                 relationAlternation='|'.join(r.n3() for r in relations))
+        elif base_type:
+            selector = self.type_selector.format(graph=graph.n3() if graph else '?graph',
+                                                 baseType=base_type.n3())
+
+        excluded_types = excluded_types or [expand(t) for t in request.GET.getlist('exclude_type')]
+        properties = properties or [expand(p) for p in request.GET.getlist('property')]
+
         page_uri = rdflib.URIRef(request.build_absolute_uri())
-        
-        query = self._QUERY % {'subject': subj,
-                               'object': obj,
-                               'depth': depth,
-                               'relationList': ', '.join(r.n3() for r in relations),
-                               'excludeTypesFilter': ('FILTER (?type not in (%s)) .' % ', '.join(t.n3() for t in exclude_types)) if exclude_types else '',
-                               'relationAlternation': '|'.join(r.n3() for r in relations),
-                               'relationPattern': relation_pattern,
-                               'page_uri': page_uri.n3(),
-                               'propertyPatterns': '\n        '.join('OPTIONAL { ?entity %s ?p%s } .' % (p.n3(), i) for i, p in enumerate(properties)),
-                               'propertyTriples': ''.join(';\n          %s ?p%s' % (p.n3(), i) for i, p in enumerate(properties))
-                               }
+
+        query = self.query.format(selector=selector,
+                                  relations=' '.join(r.n3() for r in relations),
+                                  excludedTypes=' '.join(t.n3() for t in excluded_types),
+                                  relationPattern=relation_pattern,
+                                  page_uri=page_uri.n3(),
+                                  propertyPatterns='\n        '.join('OPTIONAL { ?entity %s ?p%s } .' % (p.n3(), i) for i, p in enumerate(properties)),
+                                  propertyTriples=''.join(';\n          %s ?p%s' % (p.n3(), i) for i, p in enumerate(properties))
+                               )
         graph = self.endpoint.query(query)
-        
+
         context = {
             'graph': graph,
             'queries': [graph.query],
@@ -78,9 +116,8 @@ class GraphVizView(RDFView):
             'relations': relations,
             'minimal': minimal,
         }
-        
-        for subject in context['subjects']:
 
+        for subject in context['subjects']:
             if not inverted:
                 subject.children = set(Resource(s, graph, self.endpoint) for relation in relations for s in graph.objects(subject._identifier, relation))
             else:
@@ -88,7 +125,7 @@ class GraphVizView(RDFView):
             for child in subject.children:
                 if (page_uri, NS['foaf'].topic, child._identifier) in graph:
                     child.display = True
-       
+
         return self.render(request, context, template)
 
     _DOT_LAYOUTS = "circo dot fdp neato nop nop1 nop2 osage patchwork sfdp twopi".split()
@@ -102,7 +139,7 @@ class GraphVizView(RDFView):
         dict(format='pdf', mimetypes=('application/pdf',), name='PDF', dot_output='pdf'),
         dict(format='svg', mimetypes=('image/svg+xml',), name='SVG', dot_output='svg'),
     ]
-    
+
     def _get_dot_renderer(output):
         def dot_renderer(self, request, context, template_name):
             layout = request.GET.get('layout')
@@ -115,15 +152,14 @@ class GraphVizView(RDFView):
             response = HttpResponse(dot_stdout, mimetype=output['mimetypes'][0])
             response['Content-Disposition'] = 'inline; filename="%s.%s"' % (slugify(context['subject'].dcterms_title)[:32], output['format'])
             return response
-        
+
         dot_output = output.pop('dot_output')
         dot_renderer.__name__ = 'render_%s' % output['format']
         return renderer(**output)(dot_renderer)
-    
+
     for output in _DOT_OUTPUTS:
         locals()['render_%s' % output['format']] = _get_dot_renderer(output)
     del _get_dot_renderer, output
-
 
     @renderer(format="gv", mimetypes=('text/vnd.graphviz',), name="DOT (GraphViz)")
     def render_gv(self, request, context, template_name):
