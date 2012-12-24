57de2435edb535fa6c0eb522be68a64e4167ddad
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2012-12-24 10:40:12 +0000

    More CORS support (allow-headers, allow-methods)

--- humfrey/base/middleware.py
+++ humfrey/base/middleware.py
@@ -7,5 +7,14 @@ class AccessControlAllowOriginMiddleware(object):
     """
 
     def process_response(self, request, response):
-        response['Access-Control-Allow-Origin'] = '*'
+        response['Access-Control-Allow-Origin'] = request.META.get('HTTP_ORIGIN', '*')
+        response['Access-Control-Expose-Headers'] = 'WWW-Authenticate'
+        if request.method == 'OPTIONS':
+            allow_methods = response.get('Accept', request.META.get('HTTP_ACCESS_CONTROL_REQUEST_METHOD'))
+            if allow_methods:
+                response['Access-Control-Allow-Methods'] = allow_methods
+            allow_headers = request.META.get('HTTP_ACCESS_CONTROL_REQUEST_HEADERS')
+            if allow_headers:
+                response['Access-Control-Allow-Headers'] = allow_headers
+        
         return response
