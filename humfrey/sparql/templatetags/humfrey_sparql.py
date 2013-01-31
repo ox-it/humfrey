5b1b15e0f64b5c9a2e152c779fe9d7a83d77f311
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2013-01-31 14:08:19 +0000

    Added template filter for turning strings into n3-encoded urirefs

--- humfrey/sparql/templatetags/humfrey_sparql.py
+++ humfrey/sparql/templatetags/humfrey_sparql.py
@@ -1,4 +1,7 @@
+import rdflib
+
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -14,3 +17,6 @@ def can_query_store(store, user):
 def can_update_store(store, user):
     return store.can_update(user)
 
+@register.filter
+def uri(v):
+    return mark_safe(rdflib.URIRef(v).n3())
