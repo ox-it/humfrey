b8f35335c2c127257a8a95ea49892ef907927b4a
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2013-07-06 22:46:20 +0100

    Trying to tidy elasticsearch.views, but need nasty hack in the meantime.

--- humfrey/elasticsearch/forms.py
+++ humfrey/elasticsearch/forms.py
@@ -4,3 +4,16 @@ class SearchForm(forms.Form):
     q = forms.CharField(label="Query")
     page = forms.IntegerField(required=False)
     page_size = forms.IntegerField(required=False)
+    default_operator = forms.ChoiceField(required=False, choices=(('and', 'and'), ('or', 'or')))
+    
+    def clean(self):
+        cleaned_data = super(SearchForm, self).clean()
+        if 'default_operator' not in cleaned_data:
+            cleaned_data['default_operator'] = 'and'
+        if not cleaned_data.get('page'):
+            cleaned_data['page'] = 1
+        if not cleaned_data.get('page_size'):
+            cleaned_data['page_size'] = 10
+        cleaned_data['from'] = (cleaned_data['page'] - 1) * cleaned_data['page_size']
+        return cleaned_data
+       
--- humfrey/elasticsearch/views.py
+++ humfrey/elasticsearch/views.py
@@ -25,6 +25,217 @@ from .forms import SearchForm
 from .query import ElasticSearchEndpoint
 from .opensearch import OpenSearchView
 
+class ElasticSearchView(HTMLView, JSONPView, MappingView, StoreView):
+    def get(self, request):
+        query = self.get_query()
+        if query is not None:
+            results = self.get_results(query)
+            self.context.update(results)
+        self.finalize_context()
+        return self.render()
+
+    @property
+    def search_endpoint(self):
+        try:
+            return self._search_endpoint
+        except AttributeError:
+            type_name = self.request.GET['type'] if re.match(r'^[a-z\-\d]+$', self.request.GET.get('type', '')) else None
+            self._search_endpoint = ElasticSearchEndpoint(self.store, type_name)
+            return self._search_endpoint
+
+    def get_query(self):
+        return None
+    
+    def get_results(self, query):
+        results = self.search_endpoint.query(query)
+        print results
+
+        results['q'] = query
+
+        for hit in results['hits']['hits']:
+            try:
+                hit['_url'] = doc_forwards(hit['_source']['uri'])[None]
+            except KeyError:
+                raise
+
+        return results
+
+    @classmethod
+    def strip_underscores(cls, value):
+        if isinstance(value, dict):
+            for key, subvalue in value.items():
+                if key.startswith('_'):
+                    value[key[1:]] = value.pop(key)
+                cls.strip_underscores(subvalue)
+        if isinstance(value, list):
+            for subvalue in value:
+                cls.strip_underscores(subvalue)
+
+    def finalize_context(self):
+        pass
+
+    @renderer(format="html", mimetypes=('text/html', 'application/xhtml+xml'), priority=1, name='HTML')
+    def render_html(self, request, context, template_name):
+        if 'hits' in context:
+            self.strip_underscores(context['hits'])
+        return super(ElasticSearchView, self).render_html(request, context, template_name)
+
+    @renderer(format="autocomplete", name="Autocomplete JSON")
+    def render_autocomplete(self, request, context, template_name):
+        if not context.get('hits'):
+            raise self.MissingQuery()
+        context = [{'value': hit['_source']['uri'],
+                    'altNames': '\t'.join(l for l in hit['_source'].get('altLabel', []) + hit['_source'].get('hiddenLabel', [])),
+                    'label': hit['_source']['label']} for hit in context['hits']['hits']]
+        content, mimetype = json.dumps(context), 'application/json'
+        if 'callback' in request.GET:
+            content, mimetype = [request.GET['callback'], '(', content, ');'], 'application/javascript'
+        return HttpResponse(content, mimetype=mimetype)
+
+    def error(self, request, exception, args, kwargs, status_code):
+        if isinstance(exception, self.MissingQuery):
+            return self.error_view(request,
+                                   {'error': {'status_code': 400,
+                                              'message': "Missing 'q' parameter."}},
+                                   'elasticsearch/bad_request')
+        else:
+            return super(SearchView, self).error(request, exception, args, kwargs, status_code)
+
+
+class SearchView(OpenSearchView, ElasticSearchView):
+    page_size = 10
+
+    # e.g. {'filter.category.uri': ('filter.subcategory.uri',)}
+    dependent_parameters = {}
+
+    facets = {'type': {'terms': {'field': 'type.uri',
+                                          'size': 20}}}
+    template_name = 'elasticsearch/search'
+    default_search_item_template_name = 'elasticsearch/search_item'
+    default_types = None
+    
+    class MissingQuery(Exception):
+        pass
+
+    def get(self, request):
+        self.search_form = SearchForm(request.GET or None)
+        self.context.update({'form': self.search_form,
+                             'base_url': request.build_absolute_uri(),
+                             'dependent_parameters': self.dependent_parameters,
+                             'default_search_item_template_name': self.default_search_item_template_name})
+        self.context.update(self.get_facet_labels(self.get_query()))
+        return super(SearchView, self).get(request)
+
+    def get_query(self):
+        if not self.search_form.is_valid():
+            return None
+        
+        cleaned_data = self.search_form.cleaned_data
+        
+        default_operator = cleaned_data.get('default_operator').upper()
+        if default_operator not in ('AND', 'OR'):
+            default_operator = 'AND'
+
+        query = {
+            'query': {'query_string': {'query': self.request.GET['q'],
+                                       'default_operator': default_operator}},
+            'from': cleaned_data['from'],
+            'size': cleaned_data['page_size'],
+            # A blank conjunctive filter. We'll remove this later if necessary.
+            'filter': {'and': []},
+        }
+
+        # Parse query parameters of the form 'filter.FIELDNAME'.
+        for key in list(self.request.GET):
+            parameter = self.request.GET[key]
+            if key.startswith('filter.'):
+                if not parameter:
+                    del self.request.GET[key]
+                    continue
+                elif parameter == '-':
+                    filter = {'missing': {'field': key[7:]}}
+                else:
+                    if key.endswith('.uri') and ':' in parameter:
+                        parameter = expand(parameter)
+                    filter = {'term': {key[7:]: parameter}}
+                query['filter']['and'].append(filter)
+        if not query['filter']['and']:
+            del query['filter']
+
+        if self.facets:
+            # Copy the facet definitions as we'll be playing with them shortly.
+            facets = copy.deepcopy(self.facets)
+
+            # Add facet filters for all active filters except any acting on this
+            # particular facet.
+            if 'filter' in query:
+                for facet in facets.itervalues():
+                    for filter in query['filter']['and']:
+                        if facet['terms']['field'] not in (filter.get('term') or filter['missing']):
+                            if 'facet_filter' not in facet:
+                                facet['facet_filter'] = {'and': []}
+                            facet['facet_filter']['and'].append(filter)
+            query['facets'] = facets
+
+        # If default_types set, add a filter to restrict the results.
+        if self.default_types and 'type' not in self.request.GET:
+            query['filter']['and'].append({'or': [{'type': {'value': t}} for t in self.default_types]})
+
+        return query
+
+    def get_facet_labels(self, query):
+        if not query:
+            return {}
+        results = self.context
+    	
+        facet_labels = set()
+        for key in query['facets']:
+            meta = results['facets'][key]['meta'] = query['facets'][key]
+            filter_value = self.request.GET.get('filter.%s' % query['facets'][key]['terms']['field'])
+            results['facets'][key]['filter'] = {'present': filter_value is not None,
+                                                'value': filter_value}
+            if meta['terms']['field'].endswith('.uri'):
+                for term in results['facets'][key]['terms']:
+                    facet_labels.add(term['term'])
+                    term['value'] = contract(term['term'])
+            else:
+                for term in results['facets'][key]['terms']:
+                    term['value'] = term['term']
+
+        labels = get_labels(map(rdflib.URIRef, facet_labels), endpoint=self.endpoint)
+        for key in query['facets']:
+            if results['facets'][key]['meta']['terms']['field'].endswith('.uri'):
+                for term in results['facets'][key]['terms']:
+                    uri = rdflib.URIRef(term['term'])
+                    if uri in labels:
+                        term['label'] = unicode(labels[uri])
+
+        for hit in results['hits']['hits']:
+            try:
+                hit['_url'] = doc_forwards(hit['_source']['uri'])[None]
+            except KeyError:
+                raise
+
+
+    def get_pagination(self, page_size, page, start, results):
+        page_count = int(math.ceil(results['hits']['total'] / page_size))
+        pages = set([1, page_count])
+        pages.update(p for p in range(page-5, page+6) if 1 <= p <= page_count)
+        pages = sorted(pages)
+        
+        pages_out = []
+        for p in pages:
+            
+            if pages_out and pages_out[-1] != p - 1:
+                pages_out.append(None)
+            pages_out.append(p)
+        
+        return {'page_count': max(1, page_count),
+                'start': start + 1,
+                'pages': pages_out,
+                'page': page,
+                'page_size': page_size}
+
 class SearchView(HTMLView, JSONPView, MappingView, OpenSearchView, StoreView):
     page_size = 10
 
