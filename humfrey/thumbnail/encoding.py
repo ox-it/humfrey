eecc5762f57794c7e2bf4cfeea08556693b5a48f
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2017-02-23 15:49:32 +0000

    Encode str object before hashing

--- humfrey/thumbnail/encoding.py
+++ humfrey/thumbnail/encoding.py
@@ -3,6 +3,7 @@ import urllib.request, urllib.parse, urllib.error
 
 from django.conf import settings
 
+
 def encode_parameters(url, width=None, height=None):
     params = [('key', settings.SECRET_KEY),
               ('url', url)]
@@ -11,6 +12,6 @@ def encode_parameters(url, width=None, height=None):
     if height:
         params.append(('height', height))
     query_string = urllib.parse.urlencode(params)
-    params.append(('s', sha256(query_string).hexdigest()))
+    params.append(('s', sha256(query_string.encode()).hexdigest()))
     params.pop(0) # Remove the key
     return urllib.parse.urlencode(params)
