78ba6c27f66032bdfd911435eee98044d9853e57
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2017-04-05 09:16:48 +0100

    Fixing RDF/XML serialization

--- humfrey/streaming/rdfxml.py
+++ humfrey/streaming/rdfxml.py
@@ -1,7 +1,4 @@
-import queue
 import re
-import sys
-import threading
 from xml.sax.saxutils import quoteattr, escape
 
 try: # rdflib 3.0
@@ -49,25 +46,24 @@ class RDFXMLSerializer(StreamingSerializer):
                 raise AssertionError("Unexpected predicate term: %r (%r)" % (type(p), p))
             for prefix, uri in namespaces:
                 if p.startswith(uri) and self.localpart.match(p[len(uri):]):
-                    tag_name = '%s:%s' % (prefix, p[len(uri):])
-                    yield '    <%s' % tag_name.encode('utf-8')
+                    tag_name = '{}:{}'.format(prefix, p[len(uri):])
+                    yield '    <{}'.format(tag_name)
                     break
             else:
                 match = self.localpart.search(p)
                 tag_name = p[match.start():]
-                yield '    <%s xmlns=%s' % (tag_name.encode('utf-8'),
-                                             quoteattr(p[:match.start()]).encode('utf-8'))
+                yield '    <{} xmlns={}'.format(tag_name, quoteattr(p[:match.start()]))
 
             if isinstance(o, Literal):
                 if o.language:
-                    yield ' xml:lang=%s' % quoteattr(o.language).encode('utf-8')
+                    yield ' xml:lang={}'.format(quoteattr(o.language))
                 if o.datatype:
-                    yield ' rdf:datatype=%s' % quoteattr(o.datatype).encode('utf-8')
-                yield '>%s</%s>\n' % (escape(o).encode('utf-8'), tag_name.encode('utf-8'))
+                    yield ' rdf:datatype={}'.format(quoteattr(o.datatype))
+                yield '>{}</{}>\n'.format(escape(o), tag_name)
             elif isinstance(o, BNode):
-                yield ' rdf:nodeID=%s/>\n' % quoteattr(o).encode('utf-8')
+                yield ' rdf:nodeID={}/>\n'.format(quoteattr(o))
             elif isinstance(o, URIRef):
-                yield ' rdf:resource=%s/>\n' % quoteattr(o).encode('utf-8')
+                yield ' rdf:resource={}/>\n'.format(quoteattr(o))
             else:
                 raise AssertionError("Unexpected object term: %r (%r)" % (type(o), o))
 
