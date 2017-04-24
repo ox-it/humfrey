a8d017ba1dbceb67ea91a47222d87c8256933fd0
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2017-04-24 15:01:49 +0100

    Add ability to queue updates from the admin site

--- humfrey/update/admin.py
+++ humfrey/update/admin.py
@@ -3,11 +3,26 @@ from guardian.admin import GuardedModelAdmin
 
 from .models import UpdateDefinition, UpdatePipeline, UpdateLog, Credential
 
+
 class CredentialAdmin(admin.ModelAdmin):
     list_display = ('user', 'url', 'username')
 
-admin.site.register(UpdateDefinition, GuardedModelAdmin)
+admin.site.register(Credential, CredentialAdmin)
+
+
+def queue_update(modeladmin, request, queryset):
+    for update_definition in queryset:
+        update_definition.queue(trigger='admin', user=request.user)
+queue_update.short_description = "Queue update"
+
+
+class UpdateDefinitionAdmin(GuardedModelAdmin):
+    list_display = ('slug', 'title', 'owner', 'cron_schedule', 'status', 'last_queued', 'last_started',
+                    'last_completed')
+    list_filter = ('status', 'owner')
+    actions = [queue_update]
+
+admin.site.register(UpdateDefinition, UpdateDefinitionAdmin)
 
 admin.site.register(UpdatePipeline)
 admin.site.register(UpdateLog)
-admin.site.register(Credential, CredentialAdmin)
