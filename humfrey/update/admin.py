83aa067c310130af04e05c46c5a04bc87f73fc60
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2017-04-24 15:03:25 +0100

    Queue silently; don't allow owner filtering

--- humfrey/update/admin.py
+++ humfrey/update/admin.py
@@ -12,14 +12,14 @@ admin.site.register(Credential, CredentialAdmin)
 
 def queue_update(modeladmin, request, queryset):
     for update_definition in queryset:
-        update_definition.queue(trigger='admin', user=request.user)
+        update_definition.queue(trigger='admin', user=request.user, silent=True)
 queue_update.short_description = "Queue update"
 
 
 class UpdateDefinitionAdmin(GuardedModelAdmin):
     list_display = ('slug', 'title', 'owner', 'cron_schedule', 'status', 'last_queued', 'last_started',
                     'last_completed')
-    list_filter = ('status', 'owner')
+    list_filter = ('status',)
     actions = [queue_update]
 
 admin.site.register(UpdateDefinition, UpdateDefinitionAdmin)
