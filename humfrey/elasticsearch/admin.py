b0c4b0da52ce9acc5bf022e3956035b24d7cce7d
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2017-04-25 12:47:53 +0100

    Display last elasticsearch index duration in admin

--- humfrey/elasticsearch/admin.py
+++ humfrey/elasticsearch/admin.py
@@ -16,9 +16,14 @@ queue_index.short_description = "Queue index"
 
 
 class IndexAdmin(admin.ModelAdmin):
-    list_display = ('slug', 'title', 'status', 'last_queued', 'last_started', 'last_completed', 'item_count')
+    list_display = ('slug', 'title', 'status', 'last_queued', 'last_started', 'last_completed', 'item_count', 'get_duration')
     list_filter = ('status',)
     actions = [queue_index]
 
+    def get_duration(self, index):
+        if index.last_started and index.last_completed and index.last_started < index.last_completed:
+            return (index.last_completed - index.last_started).total_seconds()
+        return ''
+    get_duration.short_description = 'duration'
 
 admin.site.register(Index, IndexAdmin)
